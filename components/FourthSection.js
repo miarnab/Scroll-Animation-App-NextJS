018f83396f5581542f90f698645ed9a2f2075178
Arnab Mitra <mitraarnab.2013@gmail.com>
2022-07-29 16:58:30 +0530

    Initial Commit

---
+++ components/FourthSection.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function FourthSection(){
+    return(
+        <div className="section-4">
+        <div className="sec4-content">
+        <h2 className="sec-title">Choose your
+      <span className="fancy-text">Framework And Upload</span></h2>
+        <p className="font-center w-80">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elementum, est id molestie condimentum, leo arcu egestas purus, vitae faucibus nisi ligula vitae dui. Nullam sit amet dolor pharetra augue volutpat tempor.</p>
+       </div>
+        <div className="illu-section">
+        </div>
+     </div>
+    )
+}
---
+++ components/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Nav(){
+    return(
+        <nav className='nav'>
+            <div className="logo"><h3>LOGO</h3></div>
+            <div className="menu">
+                <ul>
+                    <li>Features</li>
+                    <li>Templates</li>
+                    <li>Pricing</li>
+                    <li>Blog</li>
+                        <li>Docs</li>
+                    <li>Contact</li>
+                </ul>
+                <a className="btn" href="#" target="_blank" rel="noopener noreferrer">Stake <span>(50% APY)</span> </a>
+                <a className="btn" href="#" target="_blank" rel="noopener noreferrer">Deploy Now </a>
+            </div>
+      </nav>
+    )
+}
